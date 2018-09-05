1a960282b16737c6432905d92130c9945a564765
skiy <dev@skiy.net>
2018-09-05 08:13:54 +0800

    update

---
+++ comic.sql
@@ -0,0 +1,115 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: 2018-09-05 00:13:26
+-- 服务器版本： 5.7.22
+-- PHP Version: 7.2.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `comic`
+--
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `tb_books`
+--
+
+CREATE TABLE `tb_books` (
+  `id` int(11) NOT NULL COMMENT '编号',
+  `name` varchar(52) NOT NULL COMMENT '漫画名',
+  `status` int(11) NOT NULL DEFAULT '0' COMMENT '状态(0正在更新,1暂停更新,2完结)',
+  `origin_url` varchar(255) NOT NULL COMMENT '采集地址',
+  `origin_web` varchar(48) NOT NULL COMMENT '源站名',
+  `origin_book_id` int(11) NOT NULL COMMENT '源站漫画ID',
+  `updated_at` int(11) NOT NULL COMMENT '最后更新时间',
+  `created_at` int(11) NOT NULL COMMENT '创建时间'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='漫画名';
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `tb_chapter`
+--
+
+CREATE TABLE `tb_chapter` (
+  `id` int(11) NOT NULL COMMENT '编号',
+  `book_id` int(11) NOT NULL COMMENT '漫画ID',
+  `chapter_id` int(11) NOT NULL COMMENT '章节ID(话)',
+  `title` varchar(52) NOT NULL COMMENT '标题',
+  `origin_url` varchar(255) NOT NULL COMMENT '采集地址',
+  `created_at` int(11) NOT NULL COMMENT '创建时间'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='章节';
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `tb_images`
+--
+
+CREATE TABLE `tb_images` (
+  `id` int(11) NOT NULL COMMENT '编号',
+  `book_id` int(11) NOT NULL COMMENT '漫画ID',
+  `chapter_id` int(11) NOT NULL COMMENT '章节ID',
+  `image_url` varchar(255) NOT NULL COMMENT '图片地址',
+  `origin_url` int(11) NOT NULL COMMENT '采集地址',
+  `is_remote` tinyint(1) NOT NULL DEFAULT '1' COMMENT '是否远程图片',
+  `created_at` int(11) NOT NULL COMMENT '创建时间'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='漫画图库';
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `tb_books`
+--
+ALTER TABLE `tb_books`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tb_chapter`
+--
+ALTER TABLE `tb_chapter`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `cid` (`chapter_id`);
+
+--
+-- Indexes for table `tb_images`
+--
+ALTER TABLE `tb_images`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- 在导出的表使用AUTO_INCREMENT
+--
+
+--
+-- 使用表AUTO_INCREMENT `tb_chapter`
+--
+ALTER TABLE `tb_chapter`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '编号';
+
+--
+-- 使用表AUTO_INCREMENT `tb_images`
+--
+ALTER TABLE `tb_images`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '编号';
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- source/mh160.go
+++ source/mh160.go
@@ -6,23 +6,28 @@ import (
 	"log"
 	"fmt"
 	"github.com/axgle/mahonia"
-	regexp "regexp"
+	"regexp"
 	"strconv"
 )
 
 type Mh160 struct {
+	id  int
+	url string
 }
 
-func (this  *Mh160) Init() {
-	this.chapter();
+func (this *Mh160) Init() {
+	this.id = 31512
+	this.url = "https://m.mh160.com" //手机版
+	//this.url = "https://www.mh160.com" //PC版
+	this.mobileChapter()
 }
 
-func (this *Mh160) chapter() {
-	bookId := "31512"
-	bookName := "万界仙踪"
-	url := "https://m.mh160.com/kanmanhua/" + bookId + "/"
-
+/**
+	获取源码
+ */
+func (this *Mh160) fetchSource(url string) (doc *goquery.Document) {
 	res, err := http.Get(url)
+
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -34,41 +39,133 @@ func (this *Mh160) chapter() {
 	dec := mahonia.NewDecoder("gb18030")
 	rd := dec.NewReader(res.Body)
 
-	doc, err := goquery.NewDocumentFromReader(rd)
+	doc, err = goquery.NewDocumentFromReader(rd)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	doc.Find(".chapter-list ul li").Each(func(i int, s *goquery.Selection) {
+	return doc
+}
+
+/**
+	PC端获取章节
+ */
+func (this *Mh160) pcChapter() {
+	bookUrl := fmt.Sprintf("/kanmanhua/%d/", this.id)
+
+	doc := this.fetchSource(this.url + bookUrl)
+
+	var bookName string
+	doc.Find(".intro_l").Each(func(i int, s *goquery.Selection) {
+		bookName = s.Find(".title h1").Text()
+		//fmt.Println(bookName)
+	})
+
+	doc.Find("#pic-list").Each(func(i int, s *goquery.Selection) {
+		bookName = s.Find(".title h1").Text()
+		fmt.Println(bookName)
+	})
+
+	//doc.Find(".chapter-list ul li").Each(func(i int, s *goquery.Selection) { //手机版
+	doc.Find(".plist ul li").Each(func(i int, s *goquery.Selection) {
 		chapterName := s.Find("a").Text()
-		url, _ := s.Find("a").Attr("href");
+		url, _ := s.Find("a").Attr("href")
 
-		fmt.Printf("Review %d: - %s - %s \n", i, chapterName, url)
+		//fmt.Printf("Review %d: - %s - %s \n", i, chapterName, url)
 
-		this.detail(url, bookId, bookName, chapterName)
+		counts := this.countImage(url)
+		this.detail(url, bookName, chapterName, counts)
 	})
 }
 
-func (this *Mh160) detail(url, bookId, bookName, chapterName string) {
+/**
+	移动端获取章节
+ */
+func (this *Mh160) mobileChapter() {
+	bookUrl := fmt.Sprintf("/kanmanhua/%d/", this.id)
+
+	doc := this.fetchSource(this.url + bookUrl)
+
+	var bookName string
+	doc.Find(".main-bar").Each(func(i int, s *goquery.Selection) {
+		bookName = s.Find("h1").Text()
+		//fmt.Println(bookName)
+	})
+
+	doc.Find(".chapter-list ul li").Each(func(i int, s *goquery.Selection) { //手机版
+		chapterName := s.Find("a").Text()
+		url, _ := s.Find("a").Attr("href")
+
+		//fmt.Printf("Review %d: - %s - %s \n", i, chapterName, url)
+
+		counts := this.countImage(url)
+		this.detail(url, bookName, chapterName, counts)
+
+		/*
+		if i == 0 {
+			log.Fatalln(1)
+		}
+		*/
+	})
+}
+
+/**
+	获取共几话
+ */
+func (this *Mh160) countImage(url string) (counts int) {
+	fetchUrl := this.url + url
+	//fmt.Println(fetchUrl)
+
+	var err error
+	doc := this.fetchSource(fetchUrl)
+
+	doc.Find(".main-bar").Each(func(i int, s *goquery.Selection) {
+		imagePage := s.Find(".manga-page").Text()
+
+		preg := `([0-9\/]*)/([0-9\/]*)P`
+		re := regexp.MustCompile(preg)
+		test := re.FindStringSubmatch(imagePage)
+
+		if len(test) < 3 {
+			log.Fatalf("获取章节话数失败: %s %s", url, imagePage)
+		}
+
+		counts, err = strconv.Atoi(test[2])
+		if err != nil {
+			log.Fatal(err)
+		}
+	})
+
+	return counts
+}
+
+/**
+	获取漫画图片
+ */
+func (this *Mh160) detail(url, bookName, chapterName string, counts int) {
+	imgUrl := "https://mhpic5.lineinfo.cn/mh160tuku/w/%s_%d/%s_%s/"
+
 	//preg := `2[0-9-\s:]*`
 	preg := `/([0-9\/]*)/([0-9\.]*).html`
 	re := regexp.MustCompile(preg)
 	test := re.FindStringSubmatch(url)
 
-
-	fmt.Println(test[1], test[2])
-
-	imgUrl := "https://mhpic5.lineinfo.cn/mh160tuku/w/%s_%s/%s_%s/00%s.jpg"
+	//fmt.Println(test[1], test[2])
+	chapterId := test[2]
+	imgUrl1 := fmt.Sprintf(imgUrl, bookName, this.id, chapterName, chapterId) + "00%s.jpg"
+	//fmt.Println(imgUrl1)
 
 	var fix string
-	for i:=1; i<=10; i++ {
+	for i := 1; i < counts; i++ {
+		//fmt.Println(i)
 		if i < 10 {
 			fix = "0" + strconv.Itoa(i)
 		} else {
 			fix = strconv.Itoa(i)
 		}
 
-		imgUrl2 := fmt.Sprintf(imgUrl, bookName, bookId, chapterName, test[2], fix)
+		imgUrl2 := fmt.Sprintf(imgUrl1, fix)
+		//imgUrl2 := fmt.Sprintf(imgUrl, bookName, this.id, chapterName, test[2], fix)
 
 		fmt.Println(imgUrl2)
 		//break
@@ -77,6 +174,5 @@ func (this *Mh160) detail(url, bookId, bookName, chapterName string) {
 	//url := "https://www.mh160.com/kanmanhua/31512/658683.html"
 	//https://mhpic5.lineinfo.cn/mh160tuku/w/%E4%B8%87%E7%95%8C%E4%BB%99%E8%B8%AA_31512/%E7%AC%AC97%E8%AF%9D_658683/0006.jpg
 	//https://mhpic5.lineinfo.cn/mh160tuku/w/万界仙踪_31512/第97话_658683/0007.jpg
-
 
 }
